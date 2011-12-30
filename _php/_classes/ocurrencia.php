92dc60384de149e74ef6932388ed5ab0962b7b8b
jfimbett <jfimbett@gmail.com>
2011-12-29 23:11:47 -0500

    Update _php/_classes/ocurrencia.php

--- _php/_classes/ocurrencia.php
+++ _php/_classes/ocurrencia.php
@@ -9,38 +9,43 @@ class Ocurrencia {
 	//------------------------------------------ATRIBUTOS---------------------------------------------------//
 	//------------------------------------------------------------------------------------------------------//
 	
-	/*
+	/**
 	* Representa el dia de la ocurrencia del curso
 	*/
 	
 	private $dia;
 	
-	/*
+	/**
 	* Representa la hora de inicio del curso
 	*/
 	
 	private $horaInicio;
 	
-	/*
+	/**
 	* Representa el salon del curso
 	*/
 	
 	private $salon;
 	
-	/*
+	/**
 	* Representa la duración del curso
 	*/
 	
 	private $unidades_Duracion;
 	
-	/*
-	*Construye una nueva ocurrencia
-	*/
+	//------------------------------------------------------------------------------------------------------//
+	//------------------------------------------METODOS---------------------------------------------------//
+	//------------------------------------------------------------------------------------------------------//
+	
 	function __construct($ndia, $nhoraInicio, $nsalon, $nunidades_Duracion) {
 	$dia=$ndia;
 	$horaInicio=$_nhoraInicio;
 	$salon=$nsalon;
 	$unidades_Duracion=$nunidades_Duracion;
+	}
+	
+	function getdia(){
+		return $dia;
 	}
 }
 

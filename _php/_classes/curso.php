dd39a7044ec968581c52005231da78a56b90e2c7
jfimbett <jfimbett@gmail.com>
2011-12-29 23:02:25 -0500

    Update _php/_classes/curso.php

--- _php/_classes/curso.php
+++ _php/_classes/curso.php
@@ -76,21 +76,103 @@ class Curso {
 	//-------------------------------------------------------------------------------------------------------//
 	
 	/**
-	* Construye un nuevo curso
-	*/
+	 * Construye un nuevo curso
+	 * @param $ncapacidad_Total representa la capacidad total de estudiantes del nuevo curso
+	 * @param $ncodigo_Curso representa el codigo del nuevo curso
+	 * @param $ncreditos representa el número de créditos del nuevo curso
+	 * @param $ncrn representa el crn del nuevo curso
+	 * @param $ncupos_Disponibles representa los cupos disponible del nuevo curso
+	 * @param $ndepartamento representa el departamento o facultad del nuevo curso
+	 * @param $nnombre representa el nombre del nuevo curso
+	 * @param $nseccion representa la sección del nuevo curso
+	 * @param $ntipo representa el tipo del nuevo curso
+	 */
 	
 	function __construct($ncapacidad_Total, $ncodigo_Curso, $ncreditos,$ncrn, $ncupos_Disponibles, $ndepartamento, $nnombre, $nseccion, $ntipo) {
 	$capacidad_Total=$ncapacidad_Total;
 	$codigo_Curso=$ncodigo_Curso;
 	$creditos=$ncreditos;
 	$crn=$ncrn;
-	$cupos_Disponibles=$ncuposDisponibles;
+	$cupos_Disponibles=$ncupos_Disponibles;
 	$departamento=$ndepartamento;
 	$nombre=$nnombre;
 	$seccion=$nseccion;
 	$tipo=$ntipo;
 	$complementarias=new array();	
 	$ocurrencias=new array();
+	}
+	
+	function getcapacidad_Total(){
+		return $capacidad_Total;
+	}
+	
+	function getcodigo_Curso(){
+		return $codigo_Curso;
+	}
+	
+	function getcreditos(){
+		return $creditos;
+	}
+	
+	function getcrn(){
+		return $crn;
+	}
+	
+	function getcupos_Disponibles(){
+		return $cupos_Disponibles;
+	}
+	
+	function getdepartamento(){
+		return $departamento;
+	}
+	
+	function getnombre(){
+		return $nombre;
+	}
+	
+	function getseccion(){
+		return $seccion;
+	}
+	
+	function gettipo(){
+		return $tipo;
+	}
+	
+	
+	function setcapacidad_Total($ncapacidad_Total){
+		 $capacidad_Total=$ncapacidad_Total;
+	}
+	
+	function setcodigo_Curso($ncodigo_Curso){
+	 $codigo_Curso=$ncodigo_Curso;
+	}
+	
+	function setcreditos($ncreditos){
+	$creditos=$ncreditos;
+	}
+	
+	function setcrn($ncrn){
+	$crn=$ncrn;
+	}
+	
+	function setcupos_Disponibles($ncupos_Disponibles){
+	$cupos_Disponibles=$ncupos_Disponibles;
+	}
+	
+	function setdepartamento($ndepartamento){
+	 $departamento=$ndepartamento;
+	}
+	
+	function setnombre($nnombre){
+	 $nombre=$nnombre;
+	}
+	
+	function setseccion($nseccion){
+	 $seccion=$nseccion;
+	}
+	
+	function settipo($ntipo){
+	 $tipo=$ntipo;
 	}
 }
 

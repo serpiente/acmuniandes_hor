b87034981978df6545683217c1250cc4b131088e
jfimbett <jfimbett@gmail.com>
2011-12-29 22:39:42 -0500

    Update _php/_classes/curso.php

--- _php/_classes/curso.php
+++ _php/_classes/curso.php
@@ -11,67 +11,71 @@ class Curso {
 	
 	//TODO Implementar clase
 	
-	/*
+	/**
 	* Representa la capacidad total del curso medida como el numero máximo de personas que pueden tomar el curso
 	*/
 	private $capacidad_Total;
 	
-	/*
+	/**
 	* Representa el código único para cada curso, es una cadena de caracteres (e.g. ISIS-4001) donde las iniciales
 	* del principio indican el departamento responsable, el primer número indica si es de pregrado o maestría, y el
 	* resto del código identifica al curso.
 	*/
 	private $codigo_Curso;
 	
-	/*
+	/**
 	* Representa la cantidad de créditos del curso.
 	*/
 	private $creditos;
 	
-	/*
+	/**
 	* Representa el código CRN del curso, este código brinda la información necesaria para registrar un curso.
 	*/
 	private $crn;
 	
-	/*
+	/**
 	* Representa la cantidad de cupos disponibles en cada curso.
 	*/
 	private $cupos_Disponibles;
 	
-	/*
+	/**
 	* Representa el departamento o facultad dentro de la universidad responsable de la administración del curso
 	*/
 	private $departamento;
 	
-	/*
+	/**
 	* Representa el nombre completo del curso
 	*/
 	private $nombre;
 	
-	/*
+	/**
 	* Representa la sección del curso, esto se hace generalmente con el objetivo de distinguir cursos 
 	*iguales pero con diferente profesor. 
 	*/
 	private $seccion;
 	
-	/*
+	/**
 	* Representa el tipo del curso, CBU Tipo A, CBU Tipo B, ... etc
 	*/
 	private $tipo;
 	
-	/*
+	/**
 	* Representa las complementarias del curso
 	*/
 	
 	private $complementarias;
 	
-	/*
+	/**
 	* Representa las ocurrencias del curso
 	*/
 	
 	private $ocurrencias;
 	
-	/*
+	//-------------------------------------------------------------------------------------------------------//
+	//------------------------------------------METODOS------------------------------------------------------//
+	//-------------------------------------------------------------------------------------------------------//
+	
+	/**
 	* Construye un nuevo curso
 	*/
 	

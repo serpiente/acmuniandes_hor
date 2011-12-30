20e057519d1fbc30bad05c04d3593566cdd0d9fc
jfimbett <jfimbett@gmail.com>
2011-12-29 23:10:32 -0500

    Update _php/_classes/horario.php

--- _php/_classes/horario.php
+++ _php/_classes/horario.php
@@ -9,46 +9,100 @@ class Horario {
        //----------------------------------------------ATRIBUTOS-------------------------------------------------//
        //--------------------------------------------------------------------------------------------------------//
 	
-	/*
+	/**
 	* Representa los creditos totales del horario
 	*/
 	private $creditos_Totales;
 	
-	/*
+	/**
 	* Representa la fecha de creación del horario
 	*/
 	private $fechaCreacion;
 	
-	/*
+	/**
 	* Indica si el horario ha sido o no guardado
 	*/
 	private $guardado;
 	
-	/*
+	/**
 	* Representa el nombre del Horario
 	*/
 	private $nombre;
 	
-	/*
+	/**
 	* Representa el numero de cursos que tiene el horario
 	*/
 	private $num_Cursos;
 	
-	/*
+	/**
 	* Representa los cursos que posee el horario
 	*/
 	
 	private $cursos;
 	
-	/*
-	* Crea un nuevo horario
-	*/
-	function __construct($nfechaCreacion, $nnombre) {
+
+       //--------------------------------------------------------------------------------------------------------//
+       //----------------------------------------------METODOS---------------------------------------------------//
+       //--------------------------------------------------------------------------------------------------------//
+
+        function __construct($nfechaCreacion, $nnombre) {
 	
 	$fechaCreacion=$nfechaCreacion;
 	$nombre=$nnombre;
 	$cursos=new array();
 	}
+	
+	function getcreditos_Totales(){
+		return $creditos_Totales;
+	}
+	
+	function setcreditos_Totales($ncreditos_Totales){
+		$creditos_Totales=$ncreditos_Totales;
+	}
+	
+	function getfechaCreacion(){
+		return $fechaCreacion;
+	}
+	
+	function setfechaCreacion($nfechaCreacion){
+	 $fechaCreacion=$nfechaCreacion;
+	}
+	
+	function getguardado(){
+		return $guardado;
+	}
+	
+	function setguardado($nguardado){
+		$guardado=$nguardado;
+	}
+	
+	function getnombre(){
+		return $nombre;
+	}
+	
+	function setnombre($nnombre){
+		$nombre=$nnombre;
+	}
+	
+	function getnum_Cursos(){
+		return $num_Cursos;
+	}
+	
+	function setnum_Cursos($nnum_Cursos){
+		$num_Cursos=$nnum_Cursos;
+	}
+	
+	function getcursos(){
+		return $cursos;
+	}
+	
+	function setcursos($ncursos){
+		$cursos=$ncursos;
+	}
+	
+	
+	
+	
 }
 
 ?>
